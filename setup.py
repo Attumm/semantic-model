eef9c187d5f1f7f76ebab70d383e19524e7cab4a
Attumm <bijman.m.m@gmail.com>
2021-06-15 13:52:06 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+
+setup(
+    name='semantic-model',
+    author='Melvin Bijman',
+    author_email='bijman.m.m@gmail.com',
+
+    description='All data about data in one model',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    version='1.0.0',
+    py_modules=['semantic-model'],
+    license='MIT',
+
+    url='https://github.com/Attumm/semantic-model',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
